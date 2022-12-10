532827ba7fc042ccd4ff723d5857921caa14cff1
Wanjin Maglangit <wmaglangit1118@gmail.com>
2022-12-10 13:53:54 +0800

    implemented simple stack code, refactored code in queues.py

--- queues.py
+++ queues.py
@@ -2,16 +2,17 @@ from collections import deque
 from heapq import heappop,heappush
 from itertools import count
 
-class Queue:
-    def __init__(self, *elements):
-        self._elements = deque(elements)
-    
+class IterableMixin:
     def __len__(self):
         return len(self._elements)
 
     def __iter__(self):
         while len(self) > 0:
             yield self.dequeue()
+
+class Queue(IterableMixin):
+    def __init__(self, *elements):
+        self._elements = deque(elements)
 
     def enqueue(self, element):
         self._elements.append(element)
@@ -23,7 +24,7 @@ class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
 
-class PriorityQueue(Queue):
+class PriorityQueue(IterableMixin):
     def __init__(self):
         self._elements = []
         self._counter = count()
