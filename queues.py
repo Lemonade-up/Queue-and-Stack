70041196d5a0cd0e1e00969b7b3eddc0a44ad3ed
Wanjin Maglangit <wmaglangit1118@gmail.com>
2022-12-10 12:47:14 +0800

    completed code showcaseing enqueue and dequeue

--- queues.py
+++ queues.py
@@ -4,8 +4,8 @@ class Queue:
     def __init__(self):
         self._elements = deque()
 
-    def enqueue(self,element):
+    def enqueue(self, element):
         self._elements.append(element)
-    
+
     def dequeue(self):
         return self._elements.popleft()
