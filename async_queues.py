689751d46fba7761784e8ac1b95623ae54e1fdc9
Wanjin Maglangit <wmaglangit1118@gmail.com>
2022-12-16 15:13:34 +0800

    added lifo queue

--- async_queues.py
+++ async_queues.py
@@ -7,12 +7,16 @@ from bs4 import BeautifulSoup
 import sys
 from typing import NamedTuple
 
+class Job(NamedTuple):
+    url:str
+    depth: int = 1
 
 async def main(args):
     session = aiohttp.ClientSession()
     try:
         links = Counter()
         queue = asyncio.Queue()
+        # queue = asyncio.LifoQueue()
         tasks = [
             asyncio.create_task(
                 worker(
@@ -32,40 +36,10 @@ async def main(args):
         for task in tasks:
             task.cancel()
         await asyncio.gather(*tasks, return_exceptions = True)
-        
+
         display(links)
     finally: 
         await session.close()
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url")
-    parser.add_argument("-d", "--max-depth", type = int, default = 2)
-    parser.add_argument("url", "--num-workers", type = int, default = 3)
-    return parser.parse_args()
-
-def display(links):
-    for url, count in links.most_common():
-        print(f"{count:>3} {url}")
-
-if __name__ == "__main__":
-    asyncio.run(main(parse_args()))
-
-async def fetch_html(session, url):
-    async with session.get(url) as response:
-        if response.ok and response.content_type == "text/html":
-            return await response.text()
-
-def parse_links(url, html):
-    soup = BeautifulSoup(html, features = "html.parser")
-    for anchor in soup.select("a[href]"):
-        href = anchor.get("href").lower()
-        if not href.startswith("javascript:"):
-            yield urljoin(url, href)
-
-class Job(NamedTuple):
-    url:str
-    depth: int = 1
 
 async def worker(worker_id, session, queue, links, max_depth):
     print(f"[{worker_id} starting]", file = sys.stderr)
@@ -82,3 +56,29 @@ async def worker(worker_id, session, queue, links, max_depth):
             print(f"[{worker_id} {depth=} {url=}]", file = sys.stderr)
         finally:
             queue.task_done()
+
+async def fetch_html(session, url):
+    async with session.get(url) as response:
+        if response.ok and response.content_type == "text/html":
+            return await response.text()
+
+def parse_links(url, html):
+    soup = BeautifulSoup(html, features = "html.parser")
+    for anchor in soup.select("a[href]"):
+        href = anchor.get("href").lower()
+        if not href.startswith("javascript:"):
+            yield urljoin(url, href)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    parser.add_argument("-d", "--max-depth", type = int, default = 2)
+    parser.add_argument("-w", "--num-workers", type = int, default = 3)
+    return parser.parse_args()
+
+def display(links):
+    for url, count in links.most_common():
+        print(f"{count:>3} {url}")
+
+if __name__ == "__main__":
+    asyncio.run(main(parse_args()))
