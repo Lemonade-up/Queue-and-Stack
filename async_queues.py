3ea64e7be9a0b330ba7bafc049373ab158e739fd
Wanjin Maglangit <wmaglangit1118@gmail.com>
2022-12-16 14:37:34 +0800

    fetch and parse of links added

--- async_queues.py
+++ async_queues.py
@@ -2,6 +2,11 @@ import argparse
 import asyncio
 from collections import Counter
 import aiohttp
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import sys
+from typing import NamedTuple
+
 
 async def main(args):
     session = aiohttp.ClientSession()
@@ -24,3 +29,16 @@ def display(links):
 
 if __name__ == "__main__":
     asyncio.run(main(parse_args()))
+
+async def fetch_html(session, url):
+    async with session.get(url) as response:
+        if response.ok and response.content_type == "text/html":
+            return await response.text()
+
+def parse_links(url, html):
+    soup = BeautifulSoup(html, features = "html.parser")
+    for anchor in soup.select("a[href]"):
+        href = anchor.get("href").lower()
+        if not href.startswith("javascript:"):
+            yield urljoin(url, href)
+
